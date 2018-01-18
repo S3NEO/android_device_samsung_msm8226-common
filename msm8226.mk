bc1c24125f26386d69b84d09734912ffa09a58f4
Kevin F. Haggerty <haggertk@lineageos.org>
2018-01-18 04:22:35 -0700

    msm8226-common: Build the GPS HAL and copy config files
    Change-Id: I86fb9fff481b7bcf926b1ddce4f79fe0d87325a6

--- msm8226.mk
+++ msm8226.mk
@@ -99,8 +99,14 @@ PRODUCT_PACKAGES += \
     libfmjni
 
 # GPS
+PRODUCT_PACKAGES += \
+    gps.msm8226
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf 
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
