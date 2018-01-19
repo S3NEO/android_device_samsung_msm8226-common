a8daa6c1cffb347a4d0c1a825b928ccb6bc8a4f7
Paul Keith <javelinanddart@gmail.com>
2018-01-19 04:52:42 +0100

    msm8226: Enable full dex preopt
    * So, uh, it turns out we need to disable booting with all
      but 1 CPU core to fix CDMA RIL on klte (and hlte?), so
      let's enable full dex preopt so that we don't need to
      dex opt after system updates
    
    Change-Id: I6fe57f9dcd1da8245eebd51d61055d6de7ffe6cd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,10 +60,7 @@ BOARD_RIL_CLASS := ../../../$(VENDOR_PATH)/ril
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
+    WITH_DEXPREOPT := true
   endif
 endif
 
