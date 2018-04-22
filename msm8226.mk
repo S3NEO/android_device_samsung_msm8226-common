24b7cbb015bdf6a35d1cb7ae92ed53acddf28fa6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2018-04-22 06:27:15 +0530

    msm8226-common: Introduce dalvik tweaks

--- msm8226.mk
+++ msm8226.mk
@@ -73,6 +73,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m    
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.boot-dex2oat-threads=4 \
+    dalvik.vm.checkjni=false \
+    dalvik.vm.dex2oat-flags=--no-watch-dog \
+    dalvik.vm.dex2oat-threads=4 \
+    dalvik.vm.image-dex2oat-threads=4 \
+    dalvik.vm.verify-bytecode=false \
+    persist.sys.purgeable_assets=1
     
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=/vendor/lib/libqc-opt.so        
