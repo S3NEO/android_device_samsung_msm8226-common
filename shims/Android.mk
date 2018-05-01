fd1898ff7421d81cd9dae320461679ad0367f20a
PythonLimited <Leonardomeitz@gmail.com>
2018-05-01 22:08:30 +0200

    Update Android.mk

--- shims/Android.mk
+++ shims/Android.mk
@@ -7,4 +7,11 @@ LOCAL_SHARED_LIBRARIES := libstagefright_foundation liblog libmedia libcutils
 LOCAL_MODULE := libwvm_shim
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_SRC_FILES := imx75_shim
+LOCAL_MODULE := imx174_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_32_BIT_ONLY := true
+
 include $(BUILD_SHARED_LIBRARY)
