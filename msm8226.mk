550ebd29799332f8cbfef5a40555240ad4c36b95
Francescodario Cuzzocrea <bosconovic@gmail.com>
2018-06-16 10:31:33 +0200

    Disable audio enhancement for now, it has to be tuned a bit(maybe need to extract audio_effects.conf from stock or update soundfx blobs)

--- msm8226.mk
+++ msm8226.mk
@@ -41,8 +41,8 @@ PRODUCT_PACKAGES += \
     tinymix
 
 # Audio configuration
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf  
+# PRODUCT_COPY_FILES += \
+#     $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf  
     
 PRODUCT_PACKAGES += \
     libwvm_shim \
