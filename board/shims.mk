0426517b2c4748d28afa0580d3b11a89fbd08211
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-05-12 21:50:31 +0200

    msm8226-common: drop widevine shim as we are using libs from angler now

--- board/shims.mk
+++ board/shims.mk
@@ -1,4 +1,3 @@
 # Shims
 TARGET_LD_SHIM_LIBS += \
-    /system/vendor/lib/libwvm.so|libwvm_shim.so \
-    /system/lib/libcrypto.so|libboringssl-compat.so \
+    /system/lib/libcrypto.so|libboringssl-compat.so
