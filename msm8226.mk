05acf3945fd668151e6790e32f964ca1b418571a
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-05-13 22:47:54 +0200

    msm8226-common: remove device specific keylayouts

--- msm8226.mk
+++ msm8226.mk
@@ -113,8 +113,6 @@ PRODUCT_COPY_FILES += \
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/Button_Jack.kl:system/usr/keylayout/Button_Jack.kl \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
     $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
 
 # Keystore
