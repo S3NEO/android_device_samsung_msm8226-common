43acf65faa5d71ae51a6cf87c536fbcae1dfaf61
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-05-13 23:17:35 +0200

    msm8226-common: drop fm definition, move them to device specific trees

--- msm8226.mk
+++ msm8226.mk
@@ -99,11 +99,6 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
-# FM
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
