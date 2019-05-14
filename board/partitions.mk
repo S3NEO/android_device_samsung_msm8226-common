1c3ff49fbf0c076658777196dcd2756b11df787a
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-05-14 20:22:00 +0200

    msm8226-common: move partitions to device specific tree

--- board/partitions.mk
+++
@@ -1,5 +0,0 @@
-# Partitions and Vold
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-
