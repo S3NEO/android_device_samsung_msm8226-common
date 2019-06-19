499dfdac17be83fddfde43807e6ab442225328bc
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-06-19 21:39:25 +0200

    msm8226-common: tune up flags

--- board/architecture.mk
+++ board/architecture.mk
@@ -2,3 +2,5 @@
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_CPU_MEMCPY_BASE_OPT_DISABLE := true
 TARGET_CPU_VARIANT := krait
+BOARD_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+BOARD_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
