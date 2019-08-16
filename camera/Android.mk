switchtegrax1/android_device_motorola_msm8226-common

msm8226-common: camera: update library dependencies for O

camera/Android.mk
@@ -1,13 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := system/media/camera/include
+LOCAL_C_INCLUDES := \
+    frameworks/native/include \
+    system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libgui libutils libbase
+    libhardware liblog libcamera_client libgui libutils libbase libhidltransport libsensor libutils android.hidl.token@1.0-utils
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)

S3NEO/android_device_samsung_msm8226-common

camera/Android.mk
@@ -4,14 +4,18 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
+    frameworks/native/include \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+    libhardware liblog libcamera_client libutils libgui libbase libhidltransport libsensor libutils android.hidl.token@1.0-utils
 
+LOCAL_STATIC_LIBRARIES := \
+    libarect
+    
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.msm8226
 LOCAL_MODULE_TAGS := optional
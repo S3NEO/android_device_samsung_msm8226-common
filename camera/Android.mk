f8590e77ed447036249470585e577599b0bd3764
Adrian DC <radian.dc@gmail.com>
2018-02-09 19:39:02 +0000

    msm8226-common: camera: update library dependencies for O

--- camera/Android.mk
+++ camera/Android.mk
@@ -4,14 +4,18 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
+    frameworks/native/include \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+    libhardware liblog libcamera_client libutils libgui libbase libhidltransport libsensor libutils android.hidl.token@1.0-utils
 
+LOCAL_STATIC_LIBRARIES := \
+    libarect
+    
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.msm8226
 LOCAL_MODULE_TAGS := optional
