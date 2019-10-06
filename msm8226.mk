1378a70bb6e70dd9630b1ae83e2a625f025aa22f
pythonlimited <leonardomeitz@gmail.com>
2019-10-07 01:34:40 +0200

    msm8226-common: remove AdvancedDisplay
    samsung doesnt support this on msm8226
    
    Change-Id: If46ef0c805822767ccbc79325f059054f4e31725

--- msm8226.mk
+++ msm8226.mk
@@ -69,9 +69,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
 
-# Advanced Display
-PRODUCT_PACKAGES += \
-    AdvancedDisplay
 
 # Audio
 PRODUCT_PACKAGES += \
