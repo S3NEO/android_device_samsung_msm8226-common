5b25dd2a7ce88b24f0a15344d601895c083e74bd
RomanDesigner <romashkarus08@gmail.com>
2019-12-09 21:19:59 +0300

    msm8226-common: Switch bluetooth to hwbinder

--- msm8226.mk
+++ msm8226.mk
@@ -109,6 +109,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Boot animation
