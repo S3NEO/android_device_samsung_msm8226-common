d925b0625c49a6ca1bb856cbe05514b634a7983e
RomanDesigner <romashkarus08@gmail.com>
2020-03-10 18:58:14 +0300

    msm8226-common: Remake Android Go optimizations

--- msm8226.mk
+++ msm8226.mk
@@ -64,16 +64,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_short_ons_1="" \
     persist.rild.nitz_short_ons_2="" \
     persist.rild.nitz_short_ons_3="" \
-    ril.subscription.types=NV,RUIM
-    
-PRODUCT_PROPERTY_OVERRIDES += \
-    pm.dexopt.shared=quicken \
-    pm.dexopt.downgrade_after_inactive_days=10 
+    ril.subscription.types=NV,RUIM 
 
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heapsize=256m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m    
@@ -274,44 +268,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prima/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/prima/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
-# Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
-# Always preopt extracted APKs to prevent extracting out of the APK for gms
-# modules.
-PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
-
-# Use a profile based boot image for this device. Note that this is currently a
-# generic profile and not Android Go optimized.
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
-# Strip the local variable table and the local variable type table to reduce
-# the size of the system image. This has no bearing on stack traces, but will
-# leave less information available via JDWP.
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-
-# Do not generate libartd.
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-
-# Do not spin up a separate process for the network stack on go devices, use an in-process APK.
-PRODUCT_PACKAGES += InProcessNetworkStack
-
-# Disable Scudo to save RAM.
-PRODUCT_DISABLE_SCUDO := true
-
-# use the go specific handheld_core_hardware.xml from frameworks
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/go_handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
-
-# Set lowram options
+# Optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.traced.enable=1 \
-    ro.lmk.critical_upgrade=true \
-    ro.lmk.upgrade_pressure=40 \
-    ro.lmk.downgrade_pressure=60 \
-    ro.lmk.kill_heaviest_task=false \
-    ro.statsd.enable=true
+    ro.lmk.medium=700 \
+    dalvik.vm.madvise-random=true \
+    dalvik.vm.heapgrowthlimit=192m \
+	dalvik.vm.heapsize=256m
+
+# include Android Go product Makefile
+$(call inherit-product, build/target/product/go_defaults.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/samsung/msm8226-common/msm8226-common-vendor.mk)
